38b0f1431456f698142a435e2ed2d88d96e5d5be
ysmur <ysmur@yandex.ru>
2022-02-08 11:50:15 +0300

    Часть №5(частично). Текстовое поле, в которое можно ввести запрос для поиска объекта.По завершению ввода (нажатие на Enter) позиционирует карту на указанный объект. Изменена логика метода  def load_image класса App. Добавлен класс MapParams отвечающий за параметры отображаемой карты.

--- main.py
+++ main.py
@@ -3,43 +3,71 @@ import sys
 import pygame
 import geocoder
 from static_map import request_static_map
+LAT_STEP = 0.008  # Шаги при движении карты по широте и долготе
+LON_STEP = 0.002
+
+class MapParams(object):
+    """
+    Параметры подгружаемой карты
+    """
+    def __init__(self):
+        # Параметры по умолчанию.
+        self.lon = '37.620070'
+        self.lat = '55.753630'
+        self.delta = None
+        self.zoom = '7'
+        self.type = "map"
+        self.search_result = None  # Найденный объект для отображения на карте.
+        self.use_postal_code = False
+
+    def update(self, event):
+        if event.key == pygame.K_LEFT:
+            self.lon = str(float(self.lon) - LON_STEP * 2 ** (15 - float(self.zoom)))
+
+    def search_toponym(self, address):
+        code, toponym = geocoder.request_toponym(address)
+        if code == 200:
+            self.lon, self.lat = geocoder.get_coordinates(toponym)
+            self.delta = geocoder.get_delta(toponym)
 
 
 class App:
     """
     Класс отвечающий за запуск приложения. По умолчанию карта загружается на Москве
-    TODO логику ввода адреса и параметров поиска вывести в отдельный класс
-
     """
     def __init__(self):
         pygame.init()
-        self.width, self.height = 600, 500
+        self.width, self.height = 600, 480
         self.clock = pygame.time.Clock()
         self.screen = pygame.display.set_mode((self.width, self.height))
         self.fps = 50
+        self.mp = MapParams()
+        self.active = False
+        self.text = 'Moscow'
+        self.input_box = pygame.Rect(0, self.height - 30, self.height, 30)
 
     def terminate(self):
         pygame.quit()
         sys.exit()
 
-    def load_image(self, address):
-        """TODO в этом методе оставить только запрос к статик мапс
-                (вызывать с параметрами coordinates, delta, z, type, point)"""
-        code, toponym = geocoder.request_toponym(address)
-        if code == 200:
-            coords = geocoder.get_coordinates(toponym)
-            delta = geocoder.get_delta(toponym)
-            map_file = request_static_map(coords, delta)
-            image = pygame.image.load(map_file)
-        else:
-            image = pygame.Surface(self.screen.get_size())
-            image.fill(pygame.Color("lightblue"))
+    def load_image(self, mp):
 
-            font = pygame.font.Font(None, 40)
-            text = font.render(f'Ошибка запроса! Http статус: {code}', True, (230, 255, 200))
-            text_x = self.width // 2 - text.get_width() // 2
-            text_y = self.height // 2 - text.get_height() // 2
-            image.blit(text, (text_x, text_y))
+        coords = mp.lon, mp.lat
+        delta = mp.delta
+        z = mp.zoom
+        type = mp.type
+        map_file = request_static_map(coords, delta, z, type)
+        map = pygame.image.load(map_file)
+
+        image = pygame.Surface(self.screen.get_size())
+        image.fill(pygame.Color("lightblue"))
+        font = pygame.font.Font(None, 35)
+        text = font.render(self.text, True, (230, 255, 200))
+        text_x = self.width - text.get_width()
+        text_y = self.height - text.get_height()
+        image.blit(text, (text_x, text_y))
+        image.blit(map, (0, 0))
+        pygame.draw.rect(image, pygame.Color("lightblue"), self.input_box)
 
         return image
 
@@ -49,9 +77,27 @@ class App:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.terminate()
+                if event.type == pygame.KEYUP:
+                    self.mp.update(event)
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if self.input_box.collidepoint(event.pos):
+                        self.active = not self.active
+                        self.text = ''
+                    else:
+                        self.active = False
+                if event.type == pygame.KEYDOWN:
+                    if self.active:
+                        if event.key == pygame.K_RETURN:
+                            print(self.text)
+                            self.mp.search_toponym(self.text)
+                        elif event.key == pygame.K_BACKSPACE:
+                            self.text = self.text[:-1]
+                        else:
+                            self.text += event.unicode
 
             self.screen.fill(pygame.Color('lightblue'))
-            self.screen.blit(self.load_image('Москва'), (0, 0))
+            self.screen.blit(self.load_image(self.mp), (0, 0))
+
             pygame.display.flip()
             self.clock.tick(self.fps)
 
